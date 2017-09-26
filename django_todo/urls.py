5cc0a454d6cb6c535c8ed5d035e5021cc8aa850c
Richard Dalton <richard@codeinstitute.net>
2017-09-26 11:13:38 +0000

    Added Todo List Page

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from todo.views import get_todo_list
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', get_todo_list)
 ]
