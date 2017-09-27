bbba55e049d14a4a7f5464c78ea1bb8513ea2302
Richard Dalton <richard@codeinstitute.net>
2017-09-27 10:07:38 +0000

    Finished Code

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from todo.views import get_todo_list
+from todo.views import get_todo_list, create_an_item, edit_an_item, toggle_status
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', get_todo_list)
+    url(r'^$', get_todo_list),
+    url(r'^add$', create_an_item),
+    url(r'^edit/(?P<id>\d+)$', edit_an_item),
+    url(r'^toggle/(?P<id>\d+)$', toggle_status),
 ]
