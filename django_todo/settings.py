e855e6df9bc9ef0b411ec17e932ce40e7f4bfbbe
Richard Dalton <richard@codeinstitute.net>
2017-09-28 11:37:09 +0000

    Go back to sqlite to test CI

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # DATABASES = {
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 
-DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL")) }
+# DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL")) }
 
 
 # Password validation
